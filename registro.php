a3dc0dbe12d8749c88e3846ca87d114b7a18b10e
SushiGotosinhoDev <89935960+SushiGotosinhoDev@users.noreply.github.com>
2021-09-01 14:55:44 -0300

    Create registro.php

---
+++ registro.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>PUUUTA</title>
+
+    <link rel="stylesheet" href="index.css">
+    </head>
+
+  <body>
+    <script src="index.js">
+    </script>
+
+    <h1 id="title">No Fap está chegando! você será um campeão? registre-se aqui!</h1>
+    <br>
+    <form action="mainPage.php" method="GET">
+     <div id="container-form">
+      <input type="text" placeholder="Username" name="user">
+     </div>
+    </form>
+
+ </body>
+</html>
