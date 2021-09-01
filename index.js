f7aa2162fa1a891060e1dd4e7cad319c20dc9947
SushiGotosinhoDev <89935960+SushiGotosinhoDev@users.noreply.github.com>
2021-09-01 15:04:18 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function altRed(){
+   document.getElementById("nome").style.color="#ff1605"
+}
+
+function altBlue(){
+   document.getElementById("nome").style.color="#0755f0"
+}
+
+function altGreen(){
+   document.getElementById("nome").style.color="#05ff12"
+}
+
+function altYellow(){
+   document.getElementById("nome").style.color="#eaff05"
+}
